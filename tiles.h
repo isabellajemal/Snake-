c0f2aad6ddeaa57b4267ae92c9c2fd4850db4a41
isabellajemal <103383648+isabellajemal@users.noreply.github.com>
2024-12-12 11:50:36 -0500

    Add files via upload

---
+++ tiles.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(tiles)
+
+//======================================================================
+//
+//	tiles, 16x8@4, 
+//	+ palette 256 entries, not compressed
+//	+ 2 tiles not compressed
+//	Total size: 512 + 64 = 576
+//
+//	Time-stamp: 2022-04-11, 13:12:34
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_TILES_H
+#define GRIT_TILES_H
+
+#define tilesTilesLen 64
+extern const unsigned short tilesTiles[32];
+
+#define tilesPalLen 512
+extern const unsigned short tilesPal[256];
+
+#endif // GRIT_TILES_H
+
+//}}BLOCK(tiles)
